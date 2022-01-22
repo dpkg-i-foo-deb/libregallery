6872eca86e67500e980c07f34f3e7f6ce0efb86e
Mateo Estrada <mateoestradaramirez2001@gmail.com>
2022-01-22 12:38:19 -0500

    Allow zooming on the currently displayed picture

--- app/src/main/java/com/github/libregallery/fragments/PictureFragment.kt
+++ app/src/main/java/com/github/libregallery/fragments/PictureFragment.kt
@@ -4,16 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
+import com.github.chrisbanes.photoview.PhotoView
 import com.github.libregallery.R
 
 class PictureFragment (picturePath : String): Fragment()
 {
     private var picturePath : String ?= null
 
-    private var imageView : ImageView ?= null
+    private var imageView : PhotoView ?= null
 
 
     init
