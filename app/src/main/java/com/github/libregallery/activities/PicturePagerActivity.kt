3846d0963d3067c1d745d4c701d2c0bc542b53bf
Mateo Estrada <mateoestradaramirez2001@gmail.com>
2022-01-30 15:47:09 -0500

    Animate the floating action buttons

--- app/src/main/java/com/github/libregallery/activities/PicturePagerActivity.kt
+++ app/src/main/java/com/github/libregallery/activities/PicturePagerActivity.kt
@@ -1,10 +1,14 @@
 package com.github.libregallery.activities
 
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.github.libregallery.R
 import com.github.libregallery.adapters.PicturePagerAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class PicturePagerActivity : AppCompatActivity()
 {
@@ -13,24 +17,42 @@ class PicturePagerActivity : AppCompatActivity()
     private var adapter : PicturePagerAdapter ?= null
     private var viewPager : ViewPager2 ?= null
 
+    private var menuButton : FloatingActionButton ?= null
+    private var shareButton : FloatingActionButton ?= null
+
+    private var clicked : Boolean = false
+
+    private var fromBottom : Animation ?= null
+    private var toBottom : Animation ?= null
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_picture_pager)
 
-        initializeValues()
+        initializeVariables()
 
         setViewPager()
 
+        setListeners()
     }
 
 
-    private fun initializeValues()
+
+
+    private fun initializeVariables()
     {
         imagePath=intent.getStringExtra("path")
         imageIndex=intent.getIntExtra("index",-1)
         adapter = PicturePagerAdapter(supportFragmentManager,lifecycle)
         viewPager = findViewById(R.id.view_pager)
+
+        menuButton = findViewById(R.id.floating_menu_button)
+        shareButton = findViewById(R.id.floating_share_button)
+
+        fromBottom = AnimationUtils.loadAnimation(this,R.anim.from_bottom_animation)
+        toBottom = AnimationUtils.loadAnimation(this,R.anim.to_bottom_animation)
+
     }
 
     private fun setViewPager()
@@ -39,4 +61,40 @@ class PicturePagerActivity : AppCompatActivity()
         viewPager?.setCurrentItem(imageIndex!!,false)
     }
 
+    private fun setListeners()
+    {
+        menuButton?.setOnClickListener{onMenuButtonClicked()}
+    }
+
+    private fun onMenuButtonClicked()
+    {
+        setVisibility(clicked)
+        setAnimation(clicked)
+        clicked = !clicked
+    }
+
+    private fun setVisibility(clicked : Boolean)
+    {
+        if(!clicked)
+        {
+            shareButton?.visibility = View.VISIBLE
+        }
+        else
+        {
+            shareButton?.visibility = View.GONE
+        }
+    }
+
+    private fun setAnimation(clicked : Boolean)
+    {
+        if(!clicked)
+        {
+            shareButton?.startAnimation(fromBottom)
+
+        }
+        else
+        {
+            shareButton?.startAnimation(toBottom)
+        }
+    }
 }
